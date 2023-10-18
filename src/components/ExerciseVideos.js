47912f47fac2ea68d49df3c07d27539262aae027
ymiyad <miyad.youssoufali@ynov.com>
2023-10-18 13:48:13 +0200

    Youtube videos embed on detail page (pas encore de mis en forme)

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import Loader from './Loader'
+import ExerciseCard from './ExerciseCard';
+import YoutubeWindow from './YoutubeWindow';
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
     if (exerciseVideos == "") {
@@ -8,36 +10,15 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         return <Loader />;
     } else {
         return (
-            <Box sx={{ mt: { lg: "200px", xs: "20px" } }} p="20px">
+            <Box sx={{ mt: { lg: '203px', xs: '20px' } }} p="20px">
                 <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
-                    Watch <span style={{ color: '#CB8DEC', textTransform: 'capitalize' }}>{name}</span> exercise videos
+                    Watch <span style={{ color: '#CB8DEC', textTransform: 'capitalize' }}>{name}</span> tutorials
                 </Typography>
-                <Stack justifyContent={"flex-start"} flexWrap="wrap" alignItems="center"
-                    sx={{
-                        flexDirection: { lg: "row" },
-                        gap: { lg: '110px', xs: '0' },
-                    }}
-                >
-                    {console.log("videos found")}
-                    {exerciseVideos.slice(0, 3).map((item, index) => {
-                        { console.log(item, index) }
-                        <a key={index} className='exercise-video' href={`https://www.youtube.com/watch?v=${item.video.videoId}`} target="_blank" rel="noreferrer">
-
-                            {console.log(item.video.thumbnails[0].url)}
-                            {console.log("Lien : " + `https://www.youtube.com/watch?v=${item.video.videoId}`)}
-                            <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} />
-                            <Box>
-                                <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
-                                    {item.video.title}
-                                </Typography>
-                                <Typography fontSize="14px" color="#000">
-                                    {item.video.channelName}
-                                </Typography>
-                            </Box>
-                        </a>
-                    })}
+                <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
+                    {exerciseVideos.slice(0, 3).map((item) => (
+                        <YoutubeWindow videoId={item.video.videoId} />
+                    ))}
                 </Stack>
-
             </Box>
         );
     };
